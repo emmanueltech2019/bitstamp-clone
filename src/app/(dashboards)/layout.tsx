2a991d1b16a69393bee2ee0f18564835d06c9b7c
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-13 14:07:29 +0100

    about page

--- src/app/(dashboards)/layout.tsx
+++ src/app/(dashboards)/layout.tsx
@@ -6,11 +6,17 @@ import "../font.css";
 import NavBar from "./navbar/Navbar";
 import MobileBottomNavBar from "./navbar/MobileNav";
 import DesktopFooter from "./navbar/footer";
+import Head from "next/head";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mirror Stamp",
   description: "Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years",
+  icons: {
+    icon: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Path to your favicon
+    apple: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Optional: Path to your apple touch icon
+    shortcut: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Optional: Path to your shortcut icon
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+            <Head>
+        <title>Mirror Stamp</title>
+        <meta name="description" content="Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years" />
+        <meta property="og:title" content="Mirror Stamp" />
+        <meta property="og:description" content="Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years" />
+        <meta property="og:image" content="https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico" />
+        <meta property="og:url" content="https://www.mirrorstamptrading.com" />
+        <link rel="icon" href="https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico" />
+      </Head>
       <body>
       <NavBar/>
       <div className="pt-20 sm:pl-[16rem] pb-20 sm:pb-12"> 
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,11 +4,17 @@ import "../globals.css";
 import "../font.css";
 import NavBar from "../nav/NavBar";
 import Footer from "../footer/Footer"
+import Head from "next/head";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mirror Stamp",
   description: "Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years",
+  icons: {
+    icon: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Path to your favicon
+    apple: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Optional: Path to your apple touch icon
+    shortcut: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico', // Optional: Path to your shortcut icon
+  },
 };
  
 export default function RootLayout({
@@ -18,6 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Mirror Stamp</title>
+        <meta name="description" content="Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years" />
+        <meta property="og:title" content="Mirror Stamp" />
+        <meta property="og:description" content="Mirror Stamp is a group of financial and cryptocurrencies experts that mirror daily gain using shared experience of over 15 years" />
+        <meta property="og:image" content="https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico" />
+        <meta property="og:url" content="https://www.mirrorstamptrading.com" />
+        <link rel="icon" href="https://res.cloudinary.com/wise-solution-inc/image/upload/v1718283601/favicon_dpojxt.ico" />
+      </Head>
       <body>
       <NavBar/>
         {children}
