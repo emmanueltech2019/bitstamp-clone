d36c9decf6466648053cf9999d58a39376512754
Emmanuel Lucky <emmanueltech2019@gmail.com>
2024-12-11 09:09:03 +0100

    updates

--- src/app/(dashboards)/dashboard/deposit/page.tsx
+++ src/app/(dashboards)/dashboard/deposit/page.tsx
@@ -100,7 +100,7 @@ function Deposit() {
   // };
 
   const cryptoOptions = [
-    { label: 'Bitcoin (BTC)', icon: Btc, wallet:"bc1q35ekf52pgaym7lavkhckq4l29chcd6aexrup53" },
+    { label: 'Bitcoin (BTC)', icon: Btc, wallet:"bc1qv3t3nelvutvmttu3qh4jjphljd365d37c4l9k7" },
     { label: 'Ethereum (ETH)', icon: Eth, wallet:"0x4742FFebB9eD23635AA8A598d511c4F7e150dC83" },
     { label: 'Tether US (USDT)', icon: Usdt, wallet:"TXf3FUkX9WqXeC83YeHFQPgoKybgCBDs9p" },
     { label: 'Litecoin (LTC)', icon: Ltc, wallet:"ltc1qezl5hum4kfey74ahame7m23h79fngxqpegeu8s" },
