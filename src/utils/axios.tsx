abcadb747b1f986790ba0f562aa5bb55c5b3aa08
Emmanuel Lucky <emmanueltech2019@gmail.com>
2025-08-05 15:54:15 +0100

    updated

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: `http://localhost:4000/api/v1`
-  baseURL:`https://mirror.chop-life.com/api/v1`
+  baseURL:`https://mirror.chidefashion.com/api/v1`
 });
 
 export default instance;
