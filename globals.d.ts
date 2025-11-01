a9f0a560aebc1d4c53f70f3c4d74170772c8f9b2
Emmanuel Lucky <emmanueltech2019@gmail.com>
2025-11-01 16:48:17 +0100

    updated

--- globals.d.ts
+++ globals.d.ts
@@ -1,13 +1,13 @@
-// interface Window {
-//     grecaptcha: any;
-//     googleTranslateElementInit?: () => void;    
-//     Image: typeof Image;
-//     Image: new () => HTMLImageElement;
-//     _smartsupp: any;
-//     smartsupp: any;
-//     google?: {
-//         translate: {
-//           TranslateElement: new (options: object, elementId: string) => void;
-//         };
-//     }; 
-// }
+interface Window {
+    grecaptcha: any;
+    googleTranslateElementInit?: () => void;    
+    Image: typeof Image;
+    Image: new () => HTMLImageElement;
+    _smartsupp: any;
+    smartsupp: any;
+    google?: {
+        translate: {
+          TranslateElement: new (options: object, elementId: string) => void;
+        };
+    }; 
+}
